6ad80e5833d4c143328bfb1db2af063781516e1d
Coța Octavian <45451082+cotaoctavian@users.noreply.github.com>
2019-12-08 10:58:18 +0200

    Update neural network.

--- Network.py
+++ Network.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import SGD, RMSprop
+from tensorflow.keras.optimizers import SGD, RMSprop, Adadelta
 import numpy as np
 
 
@@ -12,12 +12,15 @@ class Network:
         self.list_file = [self.batch_size, self.epochs]  # creating a part of the file name
         self.learning_rate = learning_rate
 
-        self.created_files = False
         self.created_file_name = ""
+        self.created_files = False
 
-    def create_layers(self, activation_hidden_layers, activation_last_layer, weight_initializer, bias_initializer):
+    def create_layers(self, activation_hidden_layers, activation_last_layer, weight_initializer, bias_initializer,
+                      loss_function, optimizer="", optimizer_parameters=""):
+
         # creating a part of the file_name
-        self.list_file.extend([activation_hidden_layers, activation_last_layer, weight_initializer, bias_initializer])
+        self.list_file.extend([activation_hidden_layers, activation_last_layer, weight_initializer, bias_initializer,
+                               optimizer, optimizer_parameters, loss_function])
 
         # second layer
         self.model.add(Dense(8 * 2, activation=activation_hidden_layers, kernel_initializer=weight_initializer,
@@ -26,29 +29,28 @@ class Network:
         # third layer
         self.model.add(Dense(8 * 2, activation=activation_hidden_layers, kernel_initializer=weight_initializer,
                              bias_initializer=bias_initializer))
+
         # last layer
         self.model.add(Dense(2, activation=activation_last_layer, kernel_initializer=weight_initializer,
                              bias_initializer=bias_initializer))
 
-    def train(self, x, y, loss_function, optimizer="", optimizer_parameters=""):
+        # SGD
+        # self.model.compile(optimizer=SGD(lr=0.1, momentum=0.75, nesterov=True), loss=loss_function, metrics=['acc'],
+        #                   shuffle=True)
+
+        # RMS prop
+        # self.model.compile(optimizer=RMSprop(lr=0.05, rho=0.9), loss=loss_function, metrics=None)
+
+        # Adadelta
+        self.model.compile(optimizer=Adadelta(lr=0.1, rho=0.95, epsilon=1e-6), loss=loss_function, metrics=None)
+
+    def train(self, x, y):
         # convert x, y to make it work
         x = list(map(lambda d: list(d.values()), x))
         x = np.array(x)
         y = np.array(y)
 
-        # creating a part of the file_name
-        if not self.created_files:
-            self.list_file.extend([optimizer, optimizer_parameters, loss_function])
-
-        # setting up the optimizer for the neuronal network
-        # RMS prop
-        # self.model.compile(optimizer=RMSprop(lr=0.05, rho=0.9), loss=loss_function, metrics=None)
-
-        # SGD
-        self.model.compile(optimizer=SGD(lr=0.1, momentum=0.75, nesterov=True), loss=loss_function, metrics=['acc'],
-                           shuffle=True)
-
-        # first layer + start training
+        # start training
         self.model.fit(x=x, y=y, epochs=self.epochs, batch_size=self.batch_size, shuffle=True)
 
         self.save_file()
@@ -65,19 +67,17 @@ class Network:
         return output[0]
 
     def save_file(self):
-        if not self.created_files:
+        if self.created_files is False:
             for item in self.list_file:
                 if item != "":
                     self.created_file_name += str(item) + "_"
+            self.created_files = True
 
         self.model.save(filepath=self.created_file_name + "model.h5")
         self.model.save_weights(filepath=self.created_file_name + "weights.h5")
 
-        self.created_files = True
-
     def load(self, file_name):
         self.created_file_name = file_name
-        self.created_files = True
         self.model = load_model(file_name + "_model.h5")
         self.model.load_weights(file_name + "_weights.h5")
 
