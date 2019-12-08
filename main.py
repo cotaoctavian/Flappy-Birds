2273226ef1691861c3a34da2c0d191ee2576ac52
Matei-L <lipanmatei@gmail.com>
2019-12-08 20:24:47 +0200

    sa moara gibilan

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from ple import PLE
 from ple.games.flappybird import FlappyBird
+from Network import Network
 import os
 import sys
 import numpy as np
-from Network import Network
+import matplotlib.pyplot as plt
 
 game_width = 288
 game_height = 512
 game_pipe_gap = 100
+
 
 # Run Keras on CPU
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -20,10 +22,10 @@ def get_gap_size(y_bottom, y_top):
 def get_reward_relative_to_pipe(y_bird, y_bottom, y_top, delta_x, max_width):
     gap_size = get_gap_size(y_bottom, y_top)
     delta_y = np.absolute(y_bird - (y_top + gap_size / 2))
-    reward_for_getting_inside_the_gap = 1 - (delta_y / (gap_size / 2))
+    reward_for_getting_inside_the_gap = (gap_size / 2) - delta_y
 
-    if delta_x > max_width:
-        delta_x = max_width
+    # if delta_x > max_width:
+    #     delta_x = max_width
 
     # reward_weight = (max_width - delta_x) / max_width
 
@@ -43,84 +45,91 @@ def get_reward(state, first_pipe_importance=0.9):
                                                                      game_width)
 
 
-def q_learning(gamma=0.75, epsilon=0.9, buffer_size=500):
+def q_learning(file_name=None, gamma=0.9, epsilon=0.5):
     os.putenv('SDL_VIDEODRIVER', 'fbcon')
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
     game = FlappyBird(width=game_width, height=game_height, pipe_gap=game_pipe_gap)
 
-    p = PLE(game, fps=30, display_screen=False, force_fps=True)
+    p = PLE(game)
     p.init()
 
     last_state = None
-    last_action_taken_index = 0
-    last_actions_q_values = [0.5, 0.5]
-    counter = 0
-    no_of_trainings = 0
-    current_state = None
-    action_taken = None
-    reward = None
+    last_action = 0
+    last_actions_q_values = [0, 0]
     states_buffer = []
     labels_buffer = []
+    counter = 0
 
-    network = Network(mini_batch_size=32, epochs=3)
-    network.create_layers(activation_hidden_layers="relu",
-                          activation_last_layer="linear",
-                          weight_initializer="glorot_uniform",
-                          bias_initializer="glorot_uniform",
-                          loss_function="binary_crossentropy")  # creating layers
+    network = Network()
+    if file_name is not None:
+        network.load(file_name)
+    else:
+        network.create_layers(activation_hidden_layers="relu",
+                              activation_last_layer="linear",
+                              weight_initializer="glorot_uniform",
+                              bias_initializer="glorot_uniform",
+                              loss_function="binary_crossentropy")  # creating layers
 
     while 1:
         if p.game_over():
             p.reset_game()
-            if len(states_buffer) > buffer_size:
-                # train network
-                network.train(x=states_buffer[1:], y=labels_buffer[1:])
+            counter = 0
+            # plt.pause(0.01)
+            # train network
+            if states_buffer[0] is None:
+                states_buffer = states_buffer[1:]
+                labels_buffer = labels_buffer[1:]
+            network.train(x=states_buffer, y=labels_buffer)
+            network.save_file()
 
-                states_buffer.clear()
-                labels_buffer.clear()
+            states_buffer.clear()
+            labels_buffer.clear()
 
-                if epsilon > 0.1:
-                    epsilon = epsilon * 0.9
+            if epsilon > 0.1:
+                epsilon = epsilon - 0.00001
 
-                no_of_trainings += 1
-                counter = 0
+
 
         current_state = p.getGameState()
+        # plt.scatter(counter, game_height - current_state['player_y'])
 
         actions_q_values = network.Q(current_state.values())
-        action_taken_index = np.argmax(actions_q_values)
-
-        probabilities = [(1 - epsilon), epsilon]
         actions_indexes = np.arange(len(actions_q_values))
-        actions_indexes = np.delete(actions_indexes, action_taken_index)
-        actions_indexes = np.append([action_taken_index], actions_indexes)
 
-        action_taken_index = np.random.choice(actions_indexes, p=probabilities)
-        action_taken = None if action_taken_index == 0 else 119
+        optimal_action_to_take = np.argmax(actions_q_values)
+        random_action = np.random.choice(actions_indexes)
+
+        if np.random.uniform() < epsilon:
+            action = random_action
+        else:
+            action = optimal_action_to_take
 
         reward = get_reward(state=current_state)
         max_q = max(actions_q_values)
-        label = last_actions_q_values.copy()
-        label[last_action_taken_index] = reward + gamma * max_q
+
+        label = last_actions_q_values
+        label[last_action] = reward + gamma * max_q
+
         states_buffer += [last_state]
         labels_buffer += [label]
 
-        p.act(action_taken)
+        p.act(None if action == 0 else 119)
+
+        last_action = action
         last_state = current_state
-        last_action_taken_index = action_taken_index
         last_actions_q_values = actions_q_values
 
-        counter += 1
-
-        sys.stdout.write(f"\rGenerating training set {round((counter / buffer_size) * 100, 2)}% done. ")
+        sys.stdout.write(f"\rThe bird's' score is: {reward}.")
         sys.stdout.flush()
+
+        counter += 1
 
 
 def play(file_name, number_of_games=1):
     game = FlappyBird(width=game_width, height=game_height, pipe_gap=game_pipe_gap)
 
-    p = PLE(game, fps=30, display_screen=True, force_fps=True)
+    p = PLE(game, display_screen=True, force_fps=False)
     p.init()
 
     network = Network()
@@ -131,7 +140,7 @@ def play(file_name, number_of_games=1):
             p.reset_game()
         while not p.game_over():
             state = p.getGameState()
-
+            print(state)
             actions_q_values = network.Q(state.values())
             action_taken_index = np.argmax(actions_q_values)
             action_taken = None if action_taken_index == 0 else 119
@@ -143,7 +152,8 @@ option = input('Do you want to train me or see me play? (Write "learn" or "play"
 while option.lower() not in ['play', 'learn']:
     option = input('Write "learn" or "play"\n')
 if option.lower() == 'learn':
-    q_learning()
+    file = input('Where should I get the weights from?(leave empty for new network)\n')
+    q_learning(file)
 else:
     file = input('Where should I get the weights from?\n')
     number_of_games_to_play = input('How many games should I play?\n')
